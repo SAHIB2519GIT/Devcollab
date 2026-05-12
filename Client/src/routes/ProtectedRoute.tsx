6cbcdac9b52d2a5eb07a670ed228b845ee637fbd
Sahib <sahibxvivo@gmail.com>
2026-05-12 11:20:18 +0530

    feat: implement complete frontend authentication workflow

---
+++ Client/src/routes/ProtectedRoute.tsx
@@ -0,0 +1,23 @@
+import {
+  Navigate,
+} from "react-router-dom";
+
+import {
+  useAuthStore,
+} from "../store/authStore";
+
+function ProtectedRoute({
+  children,
+}: any) {
+
+  const user =
+    useAuthStore((state) => state.user);
+
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+}
+
+export default ProtectedRoute;
